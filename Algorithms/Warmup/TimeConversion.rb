46f7b85ece8177d78b0aba33a257014b0620e7c5
Sasha Tlr <sashatailor@gmail.com>
2016-02-17 14:34:35 -0500

    Adds logic to accommodate noon and midnight

--- Algorithms/Warmup/TimeConversion.rb
+++ Algorithms/Warmup/TimeConversion.rb
@@ -2,17 +2,19 @@
 
 time = gets.strip
 
-
 def time_conversion(time)
     #split time into an array
     time_array = time.split(":")
 
     #test if PM
-    if time_array[2].slice(-2..-1) == "PM"
-        time_array = time_array.map{|time| time.to_i}
-        time_array[0] += 12
+
+    if time_array[2].slice(-2..-1) == "PM" && time_array[0].to_i < 12
+        time_array[0] = time_array[0].to_i + 12
+    elsif time_array[2].slice(-2..-1) == "AM" && time_array[0].to_i == 12
+        time_array[0] = 0
     end
 
+    time_array = time_array.map{|time| time.to_i}
     time_array = time_array.map{ |time|
         time<10 ? "0" + time.to_s : time.to_s
         }.join(":")
